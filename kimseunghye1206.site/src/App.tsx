e0a261b7d2a8e2d8c88cd3d6f39e2152b8ec284c
kshksh723 <kshksh723@naver.com>
2025-12-29 16:16:37 +0900

    test

--- kimseunghye1206.site/src/App.tsx
+++ kimseunghye1206.site/src/App.tsx
@@ -1,8 +1,6 @@
-import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
@@ -11,20 +9,6 @@ function App() {
         정상영업중....
       </h1>
     </div>
-      <div>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
